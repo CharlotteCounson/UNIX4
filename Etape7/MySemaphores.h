c95b8fd85d584f295e903dbfff11d54d18adf936
Charlotte Counson <98714559+CharlotteCounson@users.noreply.github.com>
2022-12-27 09:11:50 +0100

    Delete Etape7 directory

--- Etape7/MySemaphores.h
+++
@@ -1,8 +0,0 @@
-#ifndef MY_SEMAPHORES_H
-#define MY_SEMAPHORES_H
-
-int sem_wait(int num);  //Tentative de prise BLOQUANTE
-int sem_try(int num);   //Tentative de prise NON-BLOQUANTE
-int sem_signal(int num);    //Liberation du sémaphore.
-
-#endif
